e6dcaf9112c31a6f6618e5244f141e7273718609
MerryHao <mq6721@gmail.com>
2022-12-23 13:03:05 +0800

    feat: add models and index.hbs

---
+++ models/URL.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const urlSchema = new Schema({
+  shortenURL: { type: String, required: true },
+  originalURL: { type: String, required: true },
+})
+
+module.exports = mongoose.model('URL', urlSchema)
