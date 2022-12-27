878d00f5d34ff6396696a207b9835d1e3583ce31
MerryHao <mq6721@gmail.com>
2022-12-27 16:20:32 +0800

    feat: add url shortner feature

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post("/", (req, res) => {
+  if (!req.body.url) return res.redirect("/")
+  const shortUrlGnt = shortenUrlGenerator
+
+  URL.findOne({ originalURL: req.body.url })
+    .then(data => data ? data : URL.create({ shortUrlGnt, originalURL: req.body.url }))
+    .then(data => res.render("index", { originHostPath: req.headers.origin, shortURL: data.shortURL }))
+    .catch(error => console.error(error))
+})
+
+app.get("/:shortURL", (req, res) => {
+  const { shortURL } = req.params
+  URL.findOne({ shortURL })
+    .then(data => {
+      if (!data) {
+        return res.render("error", {
+          errorMsg: "Can't found the URL",
+          errorURL: req.headers.host + "/" + shortURL,
+        })
+      }
+      res.redirect(data.originalURL)
+    })
+    .catch(error => console.error(error))
+})
 app.listen(PORT, () => {
   console.log(`Express is running on http://localhost:${PORT}`)
 })
