abe5f693e8206879b15f7d6c947f15200e766912
MerryHao <mq6721@gmail.com>
2022-12-27 19:24:58 +0800

    refactor: refactor routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exhbs = require('express-handlebars')
 require("./config/mongoose")
 const URL = require('./models/URL')
 const shortenUrlGenerator = require('./utilities/generateUrl')
+const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -10,36 +11,37 @@ app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(routes)
 
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
 
-app.post("/", (req, res) => {
-  if (!req.body.url) return res.redirect("/")
-  const shortUrlGnt = shortenUrlGenerator
+// app.post("/", (req, res) => {
+//   if (!req.body.url) return res.redirect("/")
+//   const shortUrlGnt = shortenUrlGenerator
 
-  URL.findOne({ originalURL: req.body.url })
-    .then(data => data ? data : URL.create({ shortUrlGnt, originalURL: req.body.url }))
-    .then(data => res.render("index", { originHostPath: req.headers.origin, shortURL: data.shortURL }))
-    .catch(error => console.error(error))
-})
+//   URL.findOne({ originalURL: req.body.url })
+//     .then(data => data ? data : URL.create({ shortUrlGnt, originalURL: req.body.url }))
+//     .then(data => res.render("index", { originHostPath: req.headers.origin, shortURL: data.shortURL }))
+//     .catch(error => console.error(error))
+// })
 
-app.get("/:shortURL", (req, res) => {
-  const { shortURL } = req.params
-  URL.findOne({ shortURL })
-    .then(data => {
-      if (!data) {
-        return res.render("error", {
-          errorMsg: "Can't found the URL",
-          errorURL: req.headers.host + "/" + shortURL,
-        })
-      }
-      res.redirect(data.originalURL)
-    })
-    .catch(error => console.error(error))
-})
+// app.get("/:shortURL", (req, res) => {
+//   const { shortURL } = req.params
+//   URL.findOne({ shortURL })
+//     .then(data => {
+//       if (!data) {
+//         return res.render("error", {
+//           errorMsg: "Can't found the URL",
+//           errorURL: req.headers.host + "/" + shortURL,
+//         })
+//       }
+//       res.redirect(data.originalURL)
+//     })
+//     .catch(error => console.error(error))
+// })
 app.listen(PORT, () => {
   console.log(`Express is running on http://localhost:${PORT}`)
 })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,4 +1,35 @@
 const express = require('express')
 const router = express.Router()
+const URL = require('../../models/URL')
+const shortenUrlGenerator = require('../../utilities/generateUrl')
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.post("/", (req, res) => {
+  if (!req.body.url) return res.redirect("/")
+  const shortUrlGnt = shortenUrlGenerator
+
+  URL.findOne({ originalURL: req.body.url })
+    .then(data => data ? data : URL.create({ shortUrlGnt, originalURL: req.body.url }))
+    .then(data => res.render("index", { originHostPath: req.headers.origin, shortURL: data.shortURL }))
+    .catch(error => console.error(error))
+})
+
+router.get("/:shortURL", (req, res) => {
+  const { shortURL } = req.params
+  URL.findOne({ shortURL })
+    .then(data => {
+      if (!data) {
+        return res.render("error", {
+          errorMsg: "Can't found the URL",
+          errorURL: req.headers.host + "/" + shortURL,
+        })
+      }
+      res.redirect(data.originalURL)
+    })
+    .catch(error => console.error(error))
+})
 
 module.exports = router
